c624152fe4cfae73e6dd378c406ae9b5718f1f83
Siddharth Burse <73054513+siddharthburse1@users.noreply.github.com>
2020-12-30 16:39:27 +0530

    Create AboutUs.Designer.vb

---
+++ _Frontend/AboutUs.Designer.vb
@@ -0,0 +1,137 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AboutUs
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbl_CreamAndJoyIceCreamParlor = New System.Windows.Forms.Label()
+        Me.lbl_Email = New System.Windows.Forms.Label()
+        Me.lbl_Aurangabad = New System.Windows.Forms.Label()
+        Me.lbl_ShopAdd = New System.Windows.Forms.Label()
+        Me.lbl_AboutUs = New System.Windows.Forms.Label()
+        Me.lbl_Telephone = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lbl_CreamAndJoyIceCreamParlor
+        '
+        Me.lbl_CreamAndJoyIceCreamParlor.AutoSize = True
+        Me.lbl_CreamAndJoyIceCreamParlor.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_CreamAndJoyIceCreamParlor.Font = New System.Drawing.Font("Palatino Linotype", 25.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_CreamAndJoyIceCreamParlor.ForeColor = System.Drawing.Color.AliceBlue
+        Me.lbl_CreamAndJoyIceCreamParlor.Location = New System.Drawing.Point(125, 159)
+        Me.lbl_CreamAndJoyIceCreamParlor.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_CreamAndJoyIceCreamParlor.Name = "lbl_CreamAndJoyIceCreamParlor"
+        Me.lbl_CreamAndJoyIceCreamParlor.Size = New System.Drawing.Size(656, 58)
+        Me.lbl_CreamAndJoyIceCreamParlor.TabIndex = 14
+        Me.lbl_CreamAndJoyIceCreamParlor.Text = "Cream and joy Ice-Cream Parlor"
+        '
+        'lbl_Email
+        '
+        Me.lbl_Email.AutoSize = True
+        Me.lbl_Email.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_Email.Font = New System.Drawing.Font("Palatino Linotype", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Email.ForeColor = System.Drawing.Color.AliceBlue
+        Me.lbl_Email.Location = New System.Drawing.Point(352, 440)
+        Me.lbl_Email.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_Email.Name = "lbl_Email"
+        Me.lbl_Email.Size = New System.Drawing.Size(543, 32)
+        Me.lbl_Email.TabIndex = 15
+        Me.lbl_Email.Text = "E-mail : Creamandjoyice-creamparlor@gmail.com"
+        '
+        'lbl_Aurangabad
+        '
+        Me.lbl_Aurangabad.AutoSize = True
+        Me.lbl_Aurangabad.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_Aurangabad.Font = New System.Drawing.Font("Palatino Linotype", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Aurangabad.ForeColor = System.Drawing.Color.AliceBlue
+        Me.lbl_Aurangabad.Location = New System.Drawing.Point(350, 323)
+        Me.lbl_Aurangabad.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_Aurangabad.Name = "lbl_Aurangabad"
+        Me.lbl_Aurangabad.Size = New System.Drawing.Size(214, 45)
+        Me.lbl_Aurangabad.TabIndex = 16
+        Me.lbl_Aurangabad.Text = "Aurangabad."
+        Me.lbl_Aurangabad.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lbl_ShopAdd
+        '
+        Me.lbl_ShopAdd.AutoSize = True
+        Me.lbl_ShopAdd.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_ShopAdd.Font = New System.Drawing.Font("Palatino Linotype", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_ShopAdd.ForeColor = System.Drawing.Color.AliceBlue
+        Me.lbl_ShopAdd.Location = New System.Drawing.Point(263, 252)
+        Me.lbl_ShopAdd.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_ShopAdd.Name = "lbl_ShopAdd"
+        Me.lbl_ShopAdd.Size = New System.Drawing.Size(416, 45)
+        Me.lbl_ShopAdd.TabIndex = 17
+        Me.lbl_ShopAdd.Text = "Shop No : 01, aurangpura ,"
+        Me.lbl_ShopAdd.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lbl_AboutUs
+        '
+        Me.lbl_AboutUs.AutoSize = True
+        Me.lbl_AboutUs.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_AboutUs.Font = New System.Drawing.Font("Palatino Linotype", 36.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_AboutUs.ForeColor = System.Drawing.Color.AliceBlue
+        Me.lbl_AboutUs.Location = New System.Drawing.Point(311, 27)
+        Me.lbl_AboutUs.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_AboutUs.Name = "lbl_AboutUs"
+        Me.lbl_AboutUs.Size = New System.Drawing.Size(298, 81)
+        Me.lbl_AboutUs.TabIndex = 18
+        Me.lbl_AboutUs.Text = "About Us"
+        '
+        'lbl_Telephone
+        '
+        Me.lbl_Telephone.AutoSize = True
+        Me.lbl_Telephone.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_Telephone.Font = New System.Drawing.Font("Palatino Linotype", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_Telephone.ForeColor = System.Drawing.Color.AliceBlue
+        Me.lbl_Telephone.Location = New System.Drawing.Point(13, 440)
+        Me.lbl_Telephone.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbl_Telephone.Name = "lbl_Telephone"
+        Me.lbl_Telephone.Size = New System.Drawing.Size(302, 32)
+        Me.lbl_Telephone.TabIndex = 19
+        Me.lbl_Telephone.Text = "Telephone No:0240-110011"
+        '
+        'AboutUs
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(933, 552)
+        Me.Controls.Add(Me.lbl_Telephone)
+        Me.Controls.Add(Me.lbl_AboutUs)
+        Me.Controls.Add(Me.lbl_ShopAdd)
+        Me.Controls.Add(Me.lbl_Aurangabad)
+        Me.Controls.Add(Me.lbl_Email)
+        Me.Controls.Add(Me.lbl_CreamAndJoyIceCreamParlor)
+        Me.Name = "AboutUs"
+        Me.Text = "AboutUs"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbl_CreamAndJoyIceCreamParlor As System.Windows.Forms.Label
+    Friend WithEvents lbl_Email As System.Windows.Forms.Label
+    Friend WithEvents lbl_Aurangabad As System.Windows.Forms.Label
+    Friend WithEvents lbl_ShopAdd As System.Windows.Forms.Label
+    Friend WithEvents lbl_AboutUs As System.Windows.Forms.Label
+    Friend WithEvents lbl_Telephone As System.Windows.Forms.Label
+End Class
